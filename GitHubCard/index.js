664b484450d5753559b5cf79362252e8c362d4d1
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-01-23 22:45:35 -0500

    Janky mess

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+let responseObj;
+promise = axios.get('https://api.github.com/users/5f1839842f2fb')
+  .then (response => {
+    responseObj = response;
+  })
+  .catch (error => {
 
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,7 +52,77 @@ const followersArray = [];
 </div>
 
 */
-
+cardMaker = (response) => {
+  const elementList = [
+    { //0
+      tag: 'div',
+      attributes: {class: 'card'}
+    }, 
+    { //1
+      tag: 'img',
+      attributes: {src: response.data.avatar_url},
+      parent: elementList[0]
+    }, 
+    { //2
+      tag: 'div',
+      attributes: {class: 'card-info'},
+      parent: elementList[0]
+    }, 
+    { //3
+      tag: 'h3',
+      attributes: {class: 'name'},
+      textContent: response.data.login,
+      parent: elementList[2]
+    },
+    { //4
+      tag: 'p',
+      textContent: 'Location: '+response.data.location,
+      parent: elementList[2]
+    }, 
+    { //5
+      tag: 'p',
+      textContent: 'Profile: ',
+      parent: elementList[2]
+    }, 
+    { //6
+      tag: 'a',
+      attributes: {href: response.data.html_url},
+      textContent: response.data.html_url,
+      parent: elementList[5]
+    }, 
+    { //7
+      tag: 'p',
+      textContent: 'Followers: '+response.data.followers,
+      parent: elementList[2]
+    }, 
+    { //8
+      tag: 'p',
+      textContent: 'Following: '+response.data.following,
+      parent: elementList[2]
+    },
+    { //9
+      tag: 'p',
+      textContent: 'Bio: '+response.data.bio,
+      parent: elementList[2]
+    },]
+  elementList.forEach((element, index) => {
+    let node = elementList[index];
+    node = document.createElement(node.tag)
+  })
+  console.log(elementList)
+  /* elementList[0].appendChild(elementList[1])
+  elementList[0].appendChild(elementList[2])
+  elementList[2].appendChild(elementList[3])
+  elementList[2].appendChild(elementList[4])
+  elementList[2].appendChild(elementList[5])
+  elementList[2].appendChild(elementList[6])
+  elementList[2].appendChild(elementList[7])
+  elementList[2].appendChild(elementList[8])
+  elementList[2].appendChild(elementList[9]) */
+  return elementList
+}
+elementList = cardMaker(responseObj)
+const cardsDiv = document.querySelector('.cards').appendChild(elementList[0])
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
