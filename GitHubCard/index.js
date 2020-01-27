6b8c7168cd43a8eff89fffa0b6386480660c4404
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-01-27 02:39:39 -0500

    Added 'follower' cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -7,7 +7,7 @@ promise = axios.get('https://api.github.com/users/5f1839842f2fb')
     cardMaker(response)
   })
   .catch (error => {
-
+    console.log('Something failed getting my profile')
   })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -30,8 +30,17 @@ promise = axios.get('https://api.github.com/users/5f1839842f2fb')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
+followersArray.forEach(element => {
+  promise = axios.get('https://api.github.com/users/'+element)
+  .then (response => {
+    cardMaker(response)
+  })
+  .catch (error => {
+    console.log('Something failed in the followersArray axios get')
+  })
+})
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
